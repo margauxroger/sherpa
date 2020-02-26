96a969c33dc1961b80d3aa95f2cdf538c4358707
Gaetan Brison <gaetan.brison@gmail.com>
2020-02-26 13:05:24 +0100

    api wolfram-research

---
+++ app/assets/javascripts/channels/wolfram.js
@@ -0,0 +1,14 @@
+
+// const research = () => {
+//   console.log("in research function")
+//
+//  fetch("http://api.wolframalpha.com/v2/query?input=linear%20regression&appid=73P9T2-XPL2G7VY8K")
+//   .then(response => response.json())
+//   .then((data) => {
+//     console.log(data);
+//   });
+//
+// }
+
+
+// export { research };
