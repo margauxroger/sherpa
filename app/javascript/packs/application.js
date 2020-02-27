ca21a948cb740e4f673b239aeb0520e4eeda0090
Margaux Roger <margaux831@live.fr>
2020-02-27 15:24:53 +0100

    course ok

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,7 @@ import "../components/charts_teacher_home"
 import { initMarkdown } from 'pluggins/init_markdown';
 
 initMarkdown();
+
+import { initTranslateMarkdown } from 'pluggins/init_turndown';
+
+initTranslateMarkdown();
