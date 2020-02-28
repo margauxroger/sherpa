f9a80da4a727427ee974f4dba303eec2f82876fa
Gaetan Brison <gaetan.brison@gmail.com>
2020-02-28 12:44:58 +0100

    adding interaction flashcards

--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,11 @@ Rails.application.routes.draw do
     end
   end
   resources :materials, only: [:index, :show]
-  resources :chapters, only: [:new, :create, :edit, :update]
+  resources :chapters, only: [:new, :create, :edit, :update, :destroy]
   resources :courses, only: [:show] do
     resources :flashcards, only: [:index, :create, :update, :destroy]
   end
-  resources :forums, only: [:show]
+  resources :forums, only: [:create, :update, :show] do
+    resources :messages, only: [:create, :show]
+  end
 end
