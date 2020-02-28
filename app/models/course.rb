9b56dc8706a6fe180b7662c8087ea3ffd2e26c2f
Antoine Piguet <antoine.piguet@gmail.com>
2020-02-28 13:24:35 +0100

    User pictures in photo array, api broken

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,7 @@ class Course < ApplicationRecord
   belongs_to :division
   belongs_to :material
   belongs_to :user
+
   has_many :feedbacks
+  has_many :users, through: :division
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_155912) do
+ActiveRecord::Schema.define(version: 2020_02_28_120652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 2020_02_27_155912) do
     t.bigint "division_id"
     t.string "first_name"
     t.string "last_name"
+    t.string "picture_url"
     t.index ["division_id"], name: "index_users_on_division_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
