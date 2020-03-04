08dc3bd53b4f4de38912d34b6316012d1c29caac
Antoine Piguet <antoine.piguet@gmail.com>
2020-03-04 09:53:15 +0100

    Deleted some scoring functions

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -21,19 +21,6 @@
     student_flashcards
   end
 
-
-  # A SUPPRIMER ?
-  def score_chapter(students_number)
-    score = 0
-    self.flashcards.each do |flashcard|
-      flashcard.user_answers.each do |user_answer|
-        score += user_answer.completion
-      end
-    end
-    score.fdiv(self.flashcards_number).fdiv(students_number).round(2)*100
-  end
-
-  # POUR REMPLACER
   def score_div(division)
     division.users.map { |student| student.flashcards_score(self) }.sum.fdiv(flashcards_number)
   end
