f3c90b44322fa94f0439f8d5490b8f193bbf8b5e
Gaetan Brison <gaetan.brison@gmail.com>
2020-03-05 18:45:37 +0100

    calculation of chapter score for student

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
 
   def flashcards_score_student(material)
     score = {}
-    material.chapters.each { |chapter| score[chapter.name] = flashcards_score(chapter)*20 }
+    material.chapters.each { |chapter| score[chapter.name] = (flashcards_score(chapter)/chapter.flashcards_number) * 100 }
     return score
   end
 
