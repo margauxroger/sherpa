352382dc5fe69b0f48eb555dd804b89257700403
Gaetan Brison <gaetan.brison@gmail.com>
2020-03-05 18:53:24 +0100

    v3

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
 
   def flashcards_score_student(material)
     score = {}
-    material.chapters.each { |chapter| score[chapter.name] = (flashcards_score(chapter).fdiv(chapter.flashcards_number) * 100 }
+    material.chapters.each { |chapter| score[chapter.name] = (flashcards_score(chapter).fdiv(chapter.flashcards_number)) *100 }
     return score
   end
 
