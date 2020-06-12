4156046f6ccc97d87d2fa5d78421c255df82ebd3
Margaux Roger <margaux831@live.fr>
2020-06-12 16:36:55 +0200

    starting to work on back

--- app/controllers/teachers/courses_controller.rb
+++ app/controllers/teachers/courses_controller.rb
@@ -4,10 +4,10 @@ class Teachers::CoursesController < ApplicationController
     @click_student_list  = params[:click_student_list]
     @click_forum         = params[:click_forum]
     @click_feedback_list = params[:click_feedback_list]
-    @notification        = Notification.where('user_id = ? AND course_id = ?', current_user.id, params[:id]).where(notif_type: params[:notif_type])
-    if @notification.first
-      @notification.first.mark_as_read
-    end
+    # @notification        = Notification.where('user_id = ? AND course_id = ?', current_user.id, params[:id]).where(notif_type: params[:notif_type])
+    # if @notification.first
+    #   @notification.first.mark_as_read
+    # end
 
     @course  = Course.find(params[:id])
     authorize current_user #([:teachers, @course])
@@ -38,47 +38,6 @@ class Teachers::CoursesController < ApplicationController
         @students_feedbacks << feedback if feedback.course == @course
       end
     end
-
-    @students_rating_five_stars = []
-    @students_rating_four_stars = []
-    @students_rating_three_stars = []
-    @students_rating_two_stars = []
-    @students_rating_one_stars = []
-
-    @students_feedbacks.each do |feedback|
-      if feedback.rating == 5
-        @students_rating_five_stars << feedback.rating
-      elsif feedback.rating == 4
-        @students_rating_four_stars << feedback.rating
-      elsif feedback.rating == 3
-        @students_rating_three_stars << feedback.rating
-      elsif feedback.rating == 2
-        @students_rating_two_stars << feedback.rating
-      else @students_rating_one_stars << feedback.rating
-      end
-    end
-
-    @students_rating_five_stars_percentage = @students_rating_five_stars.length.fdiv(@students_feedbacks.length).round(2) * 100
-    @students_rating_four_stars_percentage = @students_rating_four_stars.length.fdiv(@students_feedbacks.length).round(2) * 100
-    @students_rating_three_stars_percentage = @students_rating_three_stars.length.fdiv(@students_feedbacks.length).round(2) * 100
-    @students_rating_two_stars_percentage = @students_rating_two_stars.length.fdiv(@students_feedbacks.length).round(2) * 100
-    @students_rating_one_stars_percentage = @students_rating_one_stars.length.fdiv(@students_feedbacks.length).round(2) * 100
-
-    @teacher_reviews = {
-      "5 stars" => @students_rating_five_stars_percentage,
-      "4 stars" => @students_rating_four_stars_percentage,
-      "3 stars" => @students_rating_three_stars_percentage,
-      "2 stars" => @students_rating_two_stars_percentage,
-      "1 star" => @students_rating_one_stars_percentage
-    }
-
-    @sum_reviews = 0
-    @students_feedbacks.each do |feedback|
-      @sum_reviews += feedback.rating
-    end
-
-    @reviews_summary_rounded = @sum_reviews / @students_feedbacks.length
-    @reviews_summary_rounded_half = ((@sum_reviews.fdiv(@students_feedbacks.length).round(1)) * 2.0).round() / 2.0
 
     # Fin des données pour les reviews - Martin
 
@@ -115,9 +74,9 @@ class Teachers::CoursesController < ApplicationController
 
     # Données pour les alerts dans le dash de la classe - Martin
 
-    @unread_flashcards_notifications = Notification.where(user_id: current_user.id).where(notif_type: "flashcards").where(read_status: false).includes([:course])
-    @unread_feeling_notifications    = Notification.where(user_id: current_user.id).where(notif_type: "feeling").where(read_status: false).includes([:course])
-    @unread_message_notifications    = Notification.where(user_id: current_user.id).where(notif_type: "message").where(read_status: false).includes([:course])
+    # @unread_flashcards_notifications = Notification.where(user_id: current_user.id).where(notif_type: "flashcards").where(read_status: false).includes([:course])
+    # @unread_feeling_notifications    = Notification.where(user_id: current_user.id).where(notif_type: "feeling").where(read_status: false).includes([:course])
+    # @unread_message_notifications    = Notification.where(user_id: current_user.id).where(notif_type: "message").where(read_status: false).includes([:course])
 
     # Fin des données pour les alerts dash
   end
